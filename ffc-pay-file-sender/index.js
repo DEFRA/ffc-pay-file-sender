8a26f7e0f8a840e471230b9e56434a58d037eeb4
John Watson <johnwatson484@hotmail.com>
2022-03-13 00:00:40 +0000

    Rename

--- ffc-pay-file-sender/index.js
+++ ffc-pay-file-sender/index.js
@@ -2,5 +2,5 @@ const transferFile = require('./transfer-file')
 
 module.exports = async function (context, mySbMsg) {
   context.log('JavaScript ServiceBus topic trigger function received message', mySbMsg)
-  await transferFile(context, mySbMsg.filename, mySbMsg.target)
+  await transferFile(context, mySbMsg.filename, mySbMsg.ledger)
 }
