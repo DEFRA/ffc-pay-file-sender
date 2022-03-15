346ab923a5e052258b8d326068943d93bbb8d5d2
John Watson <johnwatson484@hotmail.com>
2022-03-15 21:06:00 +0000

    Remove await

--- ffc-pay-file-sender/transfer-file.js
+++ ffc-pay-file-sender/transfer-file.js
@@ -3,7 +3,7 @@ const getFile = require('./get-file')
 const storage = require('./storage')
 
 const transferFile = async (context, filename, ledger) => {
-  await storage.connect(blobConnectionString, shareConnectionString)
+  storage.connect(blobConnectionString, shareConnectionString)
   const { blob, content } = await getFile(context, filename)
   await storage.writeFile(filename, ledger, content)
   await storage.archiveFile(filename, blob)
