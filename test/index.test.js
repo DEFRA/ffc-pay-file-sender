66c452650056b371732cab35ac5dd693342b78a3
John Watson <johnwatson484@hotmail.com>
2022-10-06 21:42:14 +0100

    Tests updated

--- test/index.test.js
+++ test/index.test.js
@@ -1,19 +1,54 @@
 jest.mock('../ffc-pay-file-sender/config', () => ({ totalRetries: 1 }))
 const mockContext = require('./mock-context')
-jest.mock('../ffc-pay-file-sender/storage')
-const mockStorage = require('../ffc-pay-file-sender/storage')
+const mockContent = 'content'
+const mockGetContainerClient = jest.fn()
+const mockBlob = {
+  downloadToBuffer: jest.fn().mockResolvedValue(Buffer.from(mockContent)),
+  beginCopyFromURL: jest.fn().mockResolvedValue({ pollUntilDone: jest.fn().mockResolvedValue({ copyStatus: 'success' }) }),
+  delete: jest.fn(),
+  url: 'url'
+}
+const mockContainer = {
+  getBlockBlobClient: jest.fn().mockReturnValue(mockBlob)
+}
+const mockBlobServiceClient = {
+  getContainerClient: mockGetContainerClient.mockReturnValue(mockContainer)
+}
+jest.mock('@azure/storage-blob', () => {
+  return {
+    BlobServiceClient: {
+      fromConnectionString: jest.fn().mockReturnValue(mockBlobServiceClient)
+    }
+  }
+})
+const mockGetShareClient = jest.fn()
+const mockFile = {
+  create: jest.fn(),
+  uploadRange: jest.fn()
+}
+const mockFolder = {
+  getFileClient: jest.fn().mockReturnValue(mockFile)
+}
+const mockShare = {
+  getDirectoryClient: jest.fn().mockReturnValue(mockFolder)
+}
+const mockShareServiceClient = {
+  getShareClient: mockGetShareClient.mockReturnValue(mockShare)
+}
+jest.mock('@azure/storage-file-share', () => {
+  return {
+    ShareServiceClient: {
+      fromConnectionString: jest.fn().mockReturnValue(mockShareServiceClient)
+    }
+  }
+})
 
 const sender = require('../ffc-pay-file-sender')
-
-const blob = {}
-const content = 'content'
-
 let message
 
 describe('sender', () => {
   beforeEach(() => {
     message = { filename: 'my-file.dat', ledger: 'AP' }
-    mockStorage.getFile.mockReturnValue({ blob, content })
   })
 
   afterEach(() => {
@@ -22,16 +57,26 @@ describe('sender', () => {
 
   test('should write file to share', async () => {
     await sender(mockContext, message)
-    expect(mockStorage.writeFile).toHaveBeenCalledWith(message.filename, message.ledger, content)
+    expect(mockFile.uploadRange).toHaveBeenCalledWith(mockContent, 0, mockContent.length)
   })
 
   test('should archive original blob', async () => {
     await sender(mockContext, message)
-    expect(mockStorage.archiveFile).toHaveBeenCalledWith(message.filename, blob)
+    expect(mockBlob.beginCopyFromURL).toHaveBeenCalledWith(mockBlob.url)
+  })
+
+  test('should delete original blob location', async () => {
+    await sender(mockContext, message)
+    expect(mockBlob.delete).toHaveBeenCalledTimes(1)
+  })
+
+  test('should throw error if message schema invalid', async () => {
+    message = {}
+    await expect(sender(mockContext, message)).rejects.toThrow()
   })
 
   test('should throw error if file missing', async () => {
-    mockStorage.getFile.mockImplementation(() => { throw new Error() })
+    mockBlob.downloadToBuffer.mockImplementation(() => { throw new Error() })
     await expect(sender(mockContext, message)).rejects.toThrow()
   })
 })
