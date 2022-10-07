18ce29ccc6b006d2d89c5385330f127eb0bb8e78
John Watson <42574977+johnwatson484@users.noreply.github.com>
2022-10-07 13:17:13 +0100

    Update code coverage (#7)

--- test/retry.test.js
+++ test/retry.test.js
@@ -1,11 +1,11 @@
 jest.mock('../ffc-pay-file-sender/config', () => ({ totalRetries: 1 }))
 const retry = require('../ffc-pay-file-sender/retry')
+let mockFunction
 
 describe('retry', () => {
-  const mockFunction = jest.fn()
-
   beforeEach(() => {
     jest.clearAllMocks()
+    mockFunction = jest.fn().mockResolvedValue('success')
   })
 
   test('should retry as specified in config', async () => {
@@ -16,5 +16,30 @@ describe('retry', () => {
   test('should throw error if retries exceeded', async () => {
     mockFunction.mockImplementation(() => { throw new Error() })
     await expect(retry(mockFunction)).rejects.toThrow()
+  })
+
+  test('calls function', async () => {
+    await retry(mockFunction)
+    expect(mockFunction).toHaveBeenCalled()
+  })
+
+  test('calls function once if successful', async () => {
+    await retry(mockFunction)
+    expect(mockFunction).toHaveBeenCalledTimes(1)
+  })
+
+  test('retries function call once if fails', async () => {
+    mockFunction.mockRejectedValueOnce('error')
+    mockFunction.mockResolvedValueOnce('success')
+    await retry(mockFunction)
+    expect(mockFunction).toHaveBeenCalledTimes(2)
+  })
+
+  test('retries function call up to maximum retries', async () => {
+    mockFunction.mockRejectedValue('error')
+    try {
+      await retry(mockFunction, 1)
+    } catch {}
+    expect(mockFunction).toHaveBeenCalledTimes(2)
   })
 })
